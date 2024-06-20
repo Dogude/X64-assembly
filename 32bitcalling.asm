6dff08310d7e548903678c8b9f5bf3483f06f823
Dogude <131747396+Dogude@users.noreply.github.com>
2024-06-20 06:12:25 -0700

    Update 32bitcalling.asm

--- 32bitcalling.asm
+++ 32bitcalling.asm
@@ -1,14 +1,18 @@
- push 0
+    push 0
     push m
     push string
     push 0
     call [MessageBox]
 
-        
-    push 0
-    call [ExitProcess]
 
+   push 45
+	  push fstr
+	  call [printf]
+		
+  	push 0
+	  call [ExitProcess]
+    
 
-
-        string db "test",0
-        m db "capture!",0
+   string db "test",0
+   m db "capture!",0
+   fstr db "%d ",0
